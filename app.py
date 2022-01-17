ef59c96c66c895da1ce03473230ce3803d9dc1c0
Harrison Comfort <harrison.comfort@gmail.com>
2022-01-17 10:08:19 +0100

    trying to add update to CRUD

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template, request, redirect, url_for, jsonify
+from flask import Flask, render_template, request, redirect, url_for, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy
+import sys
+
 #render template renders html
 
 app = Flask(__name__)
@@ -11,24 +13,53 @@ class Todo(db.Model):
     __tablename__ = 'todos'
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(), nullable=False)
+    #    completed = db.Column(db.Boolean, nullable=False)
 
 def __repr__(self):
     return f'<Todo {self.id} {self.description}>'
 
 #ensures that tables are created for models we create
-#db.create_all()
+db.create_all()
 
 #controlers
 @app.route('/todos/create', methods=['POST'])
 
 def create_todo():
-   description = request.get_json()['description']
-   todo = Todo(description=description)
-   db.session.add(todo)
-   db.session.commit();
-   return jsonify({
-        'description': todo.description
-     })
+    error = False
+    body = {}
+    try:
+        description = request.get_json()['description']
+        todo  = Todo(description=description)
+        db.session.add(todo)
+        db.session.commit()
+        body['description'] = todo.description
+    except:
+        db.session.rollback()
+        error=True
+        print(sys.exc_info())
+    finally:
+        db.session.close()
+    if error:
+        abort (400)
+    else:
+        return jsonify(body)
+
+@app.route('/todos/<todo_id>/set-completed', methods=['POST'])
+def set_completed_todo(todo_id):
+    try:
+        completed = request.get_json()['completed']
+        print('completed', completed)
+        todo = Todo.query.get(todo_id)
+        todo.completed = completed
+        db.session.commit()
+    except:
+        db.session.rollback()
+    finally:
+        db.session.close()
+    if error:
+        abort (400)
+    else:
+        return redirect(url_for('index'))
 
 @app.route('/')
 #controller->
