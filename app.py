4b1a5eed8d100c12c91c4de4fbc886a5d571f57d
Harrison Comfort <harrison.comfort@gmail.com>
2022-01-17 10:21:03 +0100

    s

--- app.py
+++ app.py
@@ -44,22 +44,20 @@ def create_todo():
     else:
         return jsonify(body)
 
+
 @app.route('/todos/<todo_id>/set-completed', methods=['POST'])
 def set_completed_todo(todo_id):
-    try:
-        completed = request.get_json()['completed']
-        print('completed', completed)
-        todo = Todo.query.get(todo_id)
-        todo.completed = completed
-        db.session.commit()
-    except:
-        db.session.rollback()
-    finally:
-        db.session.close()
-    if error:
-        abort (400)
-    else:
-        return redirect(url_for('index'))
+  try:
+    completed = request.get_json()['completed']
+    print('completed', completed)
+    todo = Todo.query.get(todo_id)
+    todo.completed = completed
+    db.session.commit()
+  except:
+    db.session.rollback()
+  finally:
+    db.session.close()
+  return redirect(url_for('index'))
 
 @app.route('/')
 #controller->
