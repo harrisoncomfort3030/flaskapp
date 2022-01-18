cec8fa3ecf28ede7bbccdaa2dae038a0e783eb12
Harrison Comfort <harrison.comfort@gmail.com>
2022-01-18 16:15:15 +0100

    connecting lists with todos

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 import sys
 
 #render template renders html
@@ -10,12 +11,20 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://harrison.comfort:xxx@local
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+class TodoList(db.Model):
+    __tablename__ ="todolists"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), nullable=False)
+    todos = db.relationship('Todo', backref='list', lazy=True)
 
 class Todo(db.Model):
     __tablename__ = 'todos'
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(), nullable=False)
     completed = db.Column(db.Boolean, nullable=False, default=False)
+    list_id = db.Column(db.Integer, db.ForeignKey('todolists.id'), nullable=False)
 
 def __repr__(self):
     return f'<Todo {self.id} {self.description}>'
